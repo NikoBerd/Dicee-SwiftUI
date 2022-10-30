d005a7f3554e5ec2fa961a4c0f032fe5a7da4180
Niko Berdzenishvili <nikolozisia@gmail.com>
2022-10-30 21:43:42 +0400

    Initial Commit

---
+++ Dicee-SwiftUI/Dicee_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dicee_SwiftUIApp.swift
+//  Dicee-SwiftUI
+//
+//  Created by Niko on 30.10.22.
+//
+
+import SwiftUI
+
+@main
+struct Dicee_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
